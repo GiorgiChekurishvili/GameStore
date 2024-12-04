a05cd9f4bf077a6d39e06503de550a2ae77a0ff8
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-12-05 00:58:10 +0400

    added new DTO

---
+++ GameStore.Application/DTOs/WishlistDTO/WishlistUploadDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Application.DTOs.WishlistDTO
+{
+    public class WishlistUploadDTO
+    {
+        public int UserId { get; set; }
+        public int GameId { get; set; }
+    }
+}
