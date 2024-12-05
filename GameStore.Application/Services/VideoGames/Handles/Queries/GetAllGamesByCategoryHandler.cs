7a228e2aee35cf237b64d181a878d823806abac8
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-12-06 00:59:57 +0400

    added new query

---
+++ GameStore.Application/Services/VideoGames/Handles/Queries/GetAllGamesByCategoryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using GameStore.Application.DTOs.GameDTO;
+using GameStore.Application.Services.VideoGames.Requests.Queries;
+using GameStore.Domain.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Application.Services.VideoGames.Handles.Queries
+{
+    public class GetAllGamesByCategoryHandler : IRequestHandler<GetAllGamesByCategoryRequest, IEnumerable<GamesRetrieveDTO>>
+    {
+        readonly IGameRepository _gameRepository;
+        readonly IMapper _mapper;
+        public GetAllGamesByCategoryHandler(IGameRepository gameRepository, IMapper mapper)
+        {
+            _mapper = mapper;
+            _gameRepository = gameRepository;
+        }
+        public async Task<IEnumerable<GamesRetrieveDTO>> Handle(GetAllGamesByCategoryRequest request, CancellationToken cancellationToken)
+        {
+            var data = await _gameRepository.GetAllGamesByCategory(request.CategoryId);
+            var map = _mapper.Map<IEnumerable<GamesRetrieveDTO>>(data);
+            return map;
+        }
+    }
+}
