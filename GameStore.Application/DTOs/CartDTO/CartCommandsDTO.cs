1cfa13dd9d7347f68c7a6f62910e2e10a8a0f4a2
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-12-06 00:46:03 +0400

    Added a DTO

---
+++ GameStore.Application/DTOs/CartDTO/CartCommandsDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Application.DTOs.CartDTO
+{
+    public class CartCommandsDTO
+    {
+        public int GameId { get; set; }
+        public int UserId { get; set; }
+    }
+}
