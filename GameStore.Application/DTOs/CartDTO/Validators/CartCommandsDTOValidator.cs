4ded6830fa542f3f6a495eabd7cd7af0e2824b9b
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-12-10 23:34:01 +0400

    add CartCommandDTO Validations

---
+++ GameStore.Application/DTOs/CartDTO/Validators/CartCommandsDTOValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Application.DTOs.CartDTO.Validators
+{
+    public class CartCommandsDTOValidator : AbstractValidator<CartCommandsDTO>
+    {
+        public CartCommandsDTOValidator()
+        {
+            RuleFor(x => x.GameId).NotEmpty().WithMessage("GameId should not be empty")
+                .GreaterThan(0).WithMessage("GameId must be a positive integer");
+        }
+    }
+}
