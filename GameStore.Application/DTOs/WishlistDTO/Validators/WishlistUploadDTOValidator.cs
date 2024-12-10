92b0a2600dbee96bbf48a237e92aa35fd959fc2d
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-12-10 23:35:58 +0400

    add WIshlistUploadDTO custom validations

---
+++ GameStore.Application/DTOs/WishlistDTO/Validators/WishlistUploadDTOValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Application.DTOs.WishlistDTO.Validators
+{
+    public class WishlistUploadDTOValidator : AbstractValidator<WishlistUploadDTO>
+    {
+        public WishlistUploadDTOValidator()
+        {
+            RuleFor(x => x.GameId).NotEmpty().WithMessage("GameId Should not be empty")
+                .GreaterThan(0).WithMessage("Game Id must be a positive integer");
+        }
+    }
+}
