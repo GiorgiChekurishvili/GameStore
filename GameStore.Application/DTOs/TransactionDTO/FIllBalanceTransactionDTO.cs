b49d2c443a22e397699879046361e9cf47372b91
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-12-11 00:06:16 +0400

    add new data transfer object FillBalanceTransactionDTO

---
+++ GameStore.Application/DTOs/TransactionDTO/FIllBalanceTransactionDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Application.DTOs.TransactionDTO
+{
+    public class FIllBalanceTransactionDTO
+    {
+        public int UserId { get; set; }
+        public int Balance { get; set; }
+    }
+}
