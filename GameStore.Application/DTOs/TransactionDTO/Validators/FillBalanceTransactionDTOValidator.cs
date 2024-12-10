93596dd17297e2c3e9d07d8d9af5f6ed2fd137c6
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-12-11 00:06:40 +0400

    add new custom validation FillBalanceTransactionDTOValidator for FillBalanceTransactionDTO

---
+++ GameStore.Application/DTOs/TransactionDTO/Validators/FillBalanceTransactionDTOValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Application.DTOs.TransactionDTO.Validators
+{
+    public class FillBalanceTransactionDTOValidator : AbstractValidator<FIllBalanceTransactionDTO>
+    {
+        public FillBalanceTransactionDTOValidator()
+        {
+            RuleFor(x => x.Balance).NotEmpty().WithMessage("Balance is required to be filled")
+                .GreaterThan(0).WithMessage("input must be a positive number");
+        }
+    }
+}
