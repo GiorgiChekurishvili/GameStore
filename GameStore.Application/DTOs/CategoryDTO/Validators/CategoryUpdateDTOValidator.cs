c44ad7e596e822a07d734231d790af1fb6e38277
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-12-11 00:05:32 +0400

    add new custom validation CategoryUpdateDTOValidator

---
+++ GameStore.Application/DTOs/CategoryDTO/Validators/CategoryUpdateDTOValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Application.DTOs.CategoryDTO.Validators
+{
+    public class CategoryUpdateDTOValidator : AbstractValidator<CategoryUpdateDTO>
+    {
+        public CategoryUpdateDTOValidator()
+        {
+            RuleFor(x => x.Id).GreaterThan(0).WithMessage("Id must be a posiitve number");
+        }
+    }
+}
