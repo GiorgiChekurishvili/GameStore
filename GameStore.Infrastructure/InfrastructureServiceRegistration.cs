00a231f0979692ec33b7d273be82eec4f6fa65c6
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-12-12 00:09:41 +0400

    add service registration to connect builder services

---
+++ GameStore.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,33 @@
+﻿using GameStore.Domain.Interfaces;
+using GameStore.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<GameStoreDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<ICartRepository, CartRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IGameRepository, GameRepository>();
+            services.AddScoped<ILibraryRepository, LibraryRepository>();
+            services.AddScoped<ISystemRequirementsRepository, SystemRequirementsRepository>();
+            services.AddScoped<ITransactionRepository, TransactionRepository>();
+            services.AddScoped<IWishlistRepository, WishlistRepository>();
+
+            return services;
+        }
+    }
+}
