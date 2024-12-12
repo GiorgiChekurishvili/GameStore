8c1e6950b13db4b568883f0f81b08b1c563d14c1
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-12-13 01:44:49 +0400

    Add database connection operations in Library repository

--- GameStore.Infrastructure/Repositories/LibraryRepository.cs
+++ GameStore.Infrastructure/Repositories/LibraryRepository.cs
@@ -1,5 +1,6 @@
 ﻿using GameStore.Domain.Entities;
 using GameStore.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,14 +11,21 @@ namespace GameStore.Infrastructure.Repositories
 {
     internal class LibraryRepository : ILibraryRepository
     {
-        public Task<IEnumerable<Library>> GeAllLibraryGames()
+        readonly GameStoreDbContext _context;
+        public LibraryRepository(GameStoreDbContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
+        }
+        public async Task<IEnumerable<Library>> GeAllLibraryGames()
+        {
+            var libraryGames = await _context.Libraries.Include(x => x.Game).ToListAsync();
+            return libraryGames;
         }
 
-        public Task<IEnumerable<Library>> GetLibraryGameById(int gameId)
+        public async Task<Library> GetLibraryGameById(int gameId)
         {
-            throw new NotImplementedException();
+            var libraryGame = await _context.Libraries.Include(x=>x.Game).Where(x=>x.GameId ==  gameId).FirstOrDefaultAsync();
+            return libraryGame!;
         }
     }
 }
