defe0be435d673fc8ccc5fae425ec89f061cfe32
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-12-13 03:44:34 +0400

    change GetAllGamesByCategory handler and request from videoGames to Categories

---
+++ GameStore.Application/Services/Categories/Requests/Queries/GetAllGamesByCategoryRequest.cs
@@ -0,0 +1,16 @@
+﻿using GameStore.Application.DTOs.CategoryDTO;
+using GameStore.Application.DTOs.GameDTO;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Application.Services.Categories.Requests.Queries
+{
+    public class GetAllGamesByCategoryRequest : IRequest<IEnumerable<GameByCategoryRetrieveDTO>>
+    {
+        public int CategoryId { get; set; }
+    }
+}
