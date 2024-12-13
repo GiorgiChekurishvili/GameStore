88c95279b686942a9655617171f88d9d75140b00
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-12-14 00:54:24 +0400

    add dto-entity mappings

--- GameStore.Application/Profiles/MappingProfile.cs
+++ GameStore.Application/Profiles/MappingProfile.cs
@@ -1,5 +1,14 @@
 ﻿using AutoMapper;
+using GameStore.Application.DTOs.CartDTO;
+using GameStore.Application.DTOs.CategoryDTO;
+using GameStore.Application.DTOs.GameDTO;
+using GameStore.Application.DTOs.LibraryDTO;
+using GameStore.Application.DTOs.SystemRequirementsDTO;
+using GameStore.Application.DTOs.TransactionDTO;
+using GameStore.Application.DTOs.UserDTO;
+using GameStore.Application.DTOs.WishlistDTO;
 using GameStore.Domain.Entities;
+using GameStore.Domain.Entities.Authentication;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +21,22 @@ namespace GameStore.Application.Profiles
     {
         public MappingProfile() 
         {
-            
+            CreateMap<LoginUserDTO, User>();
+            CreateMap<RegisterUserDTO, User>();
+            CreateMap<CategoryUpdateDTO, Category>();
+            CreateMap<Category, GameByCategoryRetrieveDTO>();
+            CreateMap<Cart, CartRetrieveDTO>();
+            CreateMap<CartCommandsDTO, Cart>();
+            CreateMap<Game, GamesRetrieveDTO>();
+            CreateMap<GameUploadUpdateDTO, Game>();
+            CreateMap<Library, LibraryRetrieveDTO>();
+            CreateMap<SystemRequirement, SystemRequirementsUploadUpdateDTO>();
+            CreateMap<SystemRequirementsRetrieveDTO, SystemRequirement>();
+            CreateMap<FIllBalanceTransactionDTO, User>();
+            CreateMap<Transaction, TransactionRetrieveDTO>();
+            CreateMap<WishlistUploadDTO, Wishlist>();
+            CreateMap<Wishlist, WishlistRetrieveDTO>();
+
         }
     }
 }
