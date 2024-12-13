77efdc0e96924d066ba642f28319922b47a27250
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-12-14 00:56:18 +0400

    add AuthenticationController

---
+++ GameStore/Controllers/AuthenticationController.cs
@@ -0,0 +1,68 @@
+﻿using GameStore.Application.DTOs.UserDTO;
+using GameStore.Application.Exceptions;
+using GameStore.Application.Services.Authentication.Requests.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using ValidationException = GameStore.Application.Exceptions.ValidationException;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GameStore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        readonly IMediator _mediator;
+        public AuthenticationController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login([FromBody] LoginUserDTO loginUser)
+        {
+            try
+            {
+                var login = await _mediator.Send(new LoginRequest { UserDTO = loginUser });
+                return Ok(login);
+            }
+            catch (ValidationException ex)
+            {
+                return BadRequest(ex.Errors);
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (BadRequestException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterUserDTO registerUser)
+        {
+            try
+            {
+
+                await _mediator.Send(new RegisterRequest { User = registerUser });
+                return Ok();
+            }
+            catch (ValidationException ex)
+            {
+                return BadRequest(ex.Errors);
+            }
+            catch(NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch(BadRequestException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
