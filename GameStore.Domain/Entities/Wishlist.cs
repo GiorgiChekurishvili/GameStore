fcd6eb9d101a9fe000006232d0d2bfe64643c632
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-12-16 01:16:26 +0400

    fix typo

--- GameStore.Domain/Entities/Wishlist.cs
+++ GameStore.Domain/Entities/Wishlist.cs
@@ -12,7 +12,7 @@ namespace GameStore.Domain.Entities
     {
         public int UserId { get; set; }
         public User? User { get; set; }
-        public int GamedId { get; set; }
+        public int GameId { get; set; }
         public Game? Game { get; set; }
         public DateTime AddedOn { get; set; } = DateTime.Now;
     }
