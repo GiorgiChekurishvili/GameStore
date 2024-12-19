ad48cb1c172dab9c72f4188907bb36ab4810ebb0
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-12-19 23:36:08 +0400

    Add GameCategoryRepository to manage game what categories it have

---
+++ GameStore.Infrastructure/Repositories/GameCategoryRepository.cs
@@ -0,0 +1,43 @@
+﻿using GameStore.Domain.Entities;
+using GameStore.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Infrastructure.Repositories
+{
+    public class GameCategoryRepository : IGameCategoryRepository
+    {
+        readonly GameStoreDbContext _context;
+        public GameCategoryRepository(GameStoreDbContext context)
+        {
+            _context = context;
+        }
+        public async Task UpdateGameCategory(List<int> ids, int gameId)
+        {
+            var gameRetrieve = await _context.GameCategories.Where(x => x.GameId == gameId).ToListAsync();
+            
+            var categoriesToAdd = ids.Except(gameRetrieve.Select(x=>x.CategoryId)).ToList();
+            var categoriesToRemove = gameRetrieve.Where(x=> !ids.Contains(x.CategoryId)).ToList();
+
+            foreach ( var categoryId in categoriesToAdd )
+            {
+                var gameCategory = new GameCategory
+                {
+                    GameId = gameId,
+                    CategoryId = categoryId
+                };
+                _context.GameCategories.Add(gameCategory);
+            }
+            foreach (var category in categoriesToRemove)
+            {
+                _context.GameCategories.Remove(category);
+            }
+            await _context.SaveChangesAsync();
+
+        }
+    }
+}
