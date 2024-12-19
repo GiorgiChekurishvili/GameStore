fd72dd24ec0605b58a064ca956ada810fafda1ae
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-12-19 23:46:13 +0400

    Added validation checks before adding a game to the cart

--- GameStore.Application/Services/Carts/Handles/Commands/AddGameToCartHandler.cs
+++ GameStore.Application/Services/Carts/Handles/Commands/AddGameToCartHandler.cs
@@ -16,11 +16,13 @@ namespace GameStore.Application.Services.Carts.Handles.Commands
     public class AddGameToCartHandler : IRequestHandler<AddGameToCartRequest, Unit>
     {
         readonly ICartRepository _cartRepository;
+        readonly ILibraryRepository _libraryRepository;
         readonly IMapper _mapper;
-        public AddGameToCartHandler(ICartRepository cartRepository, IMapper mapper)
+        public AddGameToCartHandler(ICartRepository cartRepository, IMapper mapper, ILibraryRepository libraryRepository)
         {
             _cartRepository = cartRepository;
             _mapper = mapper;
+            _libraryRepository = libraryRepository;
         }
 
         public async Task<Unit> Handle(AddGameToCartRequest request, CancellationToken cancellationToken)
@@ -31,7 +33,17 @@ namespace GameStore.Application.Services.Carts.Handles.Commands
             {
                 throw new ValidationException(validationResult);
             }
-
+            var cartGames = await _cartRepository.GetCartGames(request.CartDTO!.UserId);
+            var mapCartGames = _mapper.Map<IEnumerable<Cart>>(cartGames);
+            var LibraryGames = await _libraryRepository.GetLibraryGameById(request.CartDTO.GameId, request.CartDTO.UserId);
+            if (LibraryGames != null)
+            {
+                throw new BadRequestException("Game already exists in the library.");
+            }
+            if (mapCartGames.Any(cartGame => cartGame.GameId == request.CartDTO.GameId))
+            {
+                throw new BadRequestException("Game already exists in the cart.");
+            }
             var map = _mapper.Map<Cart>(request.CartDTO);
             await _cartRepository.AddGameToCart(map);
             return Unit.Value;
