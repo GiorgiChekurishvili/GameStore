258b9ddf64516012bf93d70397343324754a4746
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-12-19 23:46:46 +0400

    Added balance validation before completing the purchase

--- GameStore.Application/Services/Carts/Handles/Commands/CheckoutGamesHandler.cs
+++ GameStore.Application/Services/Carts/Handles/Commands/CheckoutGamesHandler.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using GameStore.Application.Exceptions;
 using GameStore.Application.Services.Carts.Requests.Commands;
 using GameStore.Domain.Interfaces;
 using MediatR;
@@ -13,12 +14,20 @@ namespace GameStore.Application.Services.Carts.Handles.Commands
     public class CheckoutGamesHandler : IRequestHandler<CheckoutGamesRequest, Unit>
     {
         readonly ICartRepository _cartRepository;
-        public CheckoutGamesHandler(ICartRepository cartRepository)
+        readonly ITransactionRepository _transactionRepository;
+        public CheckoutGamesHandler(ICartRepository cartRepository, ITransactionRepository transactionRepository)
         {
             _cartRepository = cartRepository;
+            _transactionRepository = transactionRepository;
         }
         public async Task<Unit> Handle(CheckoutGamesRequest request, CancellationToken cancellationToken)
         {
+            var userBalance = await _transactionRepository.GetUserBalance(request.UserId);
+            var totalPrice = await _cartRepository.TotalPriceOfCartGames(request.UserId);
+            if (userBalance < totalPrice)
+            {
+                throw new BadRequestException("Insufficient balance to complete the purchase.");
+            }
             await _cartRepository.CheckoutGames(request.UserId);
             return Unit.Value;
         }
