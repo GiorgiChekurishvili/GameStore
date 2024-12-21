8bccc5b9e61792ec8eac7293b5436985da4efad6
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-12-21 23:26:49 +0400

    Add manual property mappings

--- GameStore.Application/Profiles/MappingProfile.cs
+++ GameStore.Application/Profiles/MappingProfile.cs
@@ -10,33 +10,34 @@ using GameStore.Application.DTOs.WishlistDTO;
 using GameStore.Domain.Entities;
 using GameStore.Domain.Entities.Authentication;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GameStore.Application.Profiles
 {
     public class MappingProfile : Profile
     {
-        public MappingProfile() 
+        public MappingProfile()
         {
             CreateMap<LoginUserDTO, User>();
             CreateMap<RegisterUserDTO, User>();
             CreateMap<CategoryUpdateDTO, Category>();
             CreateMap<Category, GameByCategoryRetrieveDTO>();
-            CreateMap<Category,CategoriesRetrieveDTO>();
+            CreateMap<Category, CategoriesRetrieveDTO>();
             CreateMap<Cart, CartRetrieveDTO>()
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Game!.Name))
                 .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Game!.Description))
                 .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Game!.Price))
                 .ForMember(dest => dest.ReleaseDate, opt => opt.MapFrom(src => src.Game!.ReleaseDate));
             CreateMap<CartCommandsDTO, Cart>();
-            CreateMap<Game, GamesRetrieveDTO>();
+            CreateMap<Game, GamesRetrieveDTO>()
+                .ForMember(dest => dest.Categories, opt => opt.MapFrom(src => src.Categories!.Select(x => x.Category!.CategoryName).ToList()));
             CreateMap<GameUploadUpdateDTO, Game>()
-                .ForMember(dest => dest.Categories, opt => opt.MapFrom(src => 
-                src.CategoryIds!.Select(id => new GameCategory { CategoryId = id}).ToList()));
-            CreateMap<Library, LibraryRetrieveDTO>();
+                .ForMember(dest => dest.Categories, opt => opt.MapFrom(src =>
+                src.CategoryIds!.Select(id => new GameCategory { CategoryId = id }).ToList()));
+            CreateMap<Library, LibraryRetrieveDTO>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Game!.Name))
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Game!.Description))
+                .ForMember(dest => dest.Categories, opt => opt.MapFrom(src => src.Game!.Categories!.Select(x=>x.Category!.CategoryName).ToList()));
             CreateMap<SystemRequirement, SystemRequirementsUploadUpdateDTO>();
             CreateMap<SystemRequirementsRetrieveDTO, SystemRequirement>();
             CreateMap<FIllBalanceTransactionDTO, User>();
