9646431be74fab468eae271a660c8341de1677f6
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-12-23 00:28:02 +0400

    Add manual mapping to Wishlist entity and WishlistRetrieveDTO

--- GameStore.Application/Profiles/MappingProfile.cs
+++ GameStore.Application/Profiles/MappingProfile.cs
@@ -46,7 +46,15 @@ namespace GameStore.Application.Profiles
             CreateMap<FIllBalanceTransactionDTO, User>();
             CreateMap<Transaction, TransactionRetrieveDTO>();
             CreateMap<WishlistUploadDTO, Wishlist>();
-            CreateMap<Wishlist, WishlistRetrieveDTO>();
+            CreateMap<Wishlist, WishlistRetrieveDTO>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Game!.Name))
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Game!.Description))
+                .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Game!.Price))
+                .ForMember(dest => dest.ReleaseDate, opt => opt.MapFrom(src => src.Game!.ReleaseDate))
+                .ForMember(dest => dest.Publisher, opt => opt.MapFrom(src => src.Game!.Publisher!.UserName))
+                .ForMember(dest => dest.Developer, opt => opt.MapFrom(src => src.Game!.Developer!.UserName))
+                .ForMember(dest => dest.GameId, opt => opt.MapFrom(src => src.Game!.Id))
+                .ForMember(dest => dest.Categories, opt => opt.MapFrom(src => src.Game!.Categories!.Select(x => x.Category!.CategoryName).ToList()));
             
 
         }
