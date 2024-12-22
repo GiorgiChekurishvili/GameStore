b8b8f4a7208f87d80d3e331f4437d8e2f97110d9
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-12-23 00:22:26 +0400

    add new properties

--- GameStore.Application/DTOs/WishlistDTO/WishlistRetrieveDTO.cs
+++ GameStore.Application/DTOs/WishlistDTO/WishlistRetrieveDTO.cs
@@ -8,12 +8,15 @@ namespace GameStore.Application.DTOs.WishlistDTO
 {
     public class WishlistRetrieveDTO
     {
+        public int Id { get; set; }
+        public int GameId { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
         public decimal Price { get; set; }
         public DateTime ReleaseDate { get; set; }
-        public int PublisherId { get; set; }
-        public int DeveloperId { get; set; }
+        public string? Publisher { get; set; }
+        public string? Developer { get; set; }
+        public IEnumerable<string>? Categories { get; set; }
         public DateTime AddedOn { get; set; }
     }
 }
