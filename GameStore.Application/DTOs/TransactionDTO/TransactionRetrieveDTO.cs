b7daded60d29b5a6452800d2adb8f1ddc86692cf
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-12-23 00:27:20 +0400

    Added Id property to TransactionRetrieveDTO

--- GameStore.Application/DTOs/TransactionDTO/TransactionRetrieveDTO.cs
+++ GameStore.Application/DTOs/TransactionDTO/TransactionRetrieveDTO.cs
@@ -8,6 +8,7 @@ namespace GameStore.Application.DTOs.TransactionDTO
 {
     public class TransactionRetrieveDTO
     {
+        public int Id { get; set; }
         public decimal TransactionsMade { get; set; }
         public string? Description { get; set; }
         public DateTime TransactionDate { get; set; }
