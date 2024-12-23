69fcd4d53ac084c8a2f5c0cdf69c9c0528cddb85
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-12-24 00:52:46 +0400

    Add cache invalidation in Redis for entity changes (create/update/delete)

---
+++ GameStore.Application/Cache/CacheService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Application.Cache
+{
+    public class CacheService : ICacheService
+    {
+        IDistributedCache _cache;
+        public CacheService(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+        public async Task RemoveCache(string key)
+        {
+            await _cache.RemoveAsync(key);
+        }
+
+        public async Task RemoveCache(string key, int id)
+        {
+            var cacheKey = $"{key}-{id}";
+            await _cache.RemoveAsync(cacheKey);
+        }
+    }
+}
