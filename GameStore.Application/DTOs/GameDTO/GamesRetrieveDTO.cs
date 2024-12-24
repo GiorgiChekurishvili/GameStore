e3b61d5e69c3709882779c4d38f3a442b4d83be9
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-12-24 13:35:56 +0400

    Added PublisherId and DeveloperId

--- GameStore.Application/DTOs/GameDTO/GamesRetrieveDTO.cs
+++ GameStore.Application/DTOs/GameDTO/GamesRetrieveDTO.cs
@@ -9,7 +9,9 @@ namespace GameStore.Application.DTOs.GameDTO
         public string? Name { get; set; }
         public string? Description { get; set; }
         public decimal Price { get; set; }
+        public int PublisherId { get; set; }
         public string? Publisher { get; set; }
+        public int DeveloperId { get; set; }
         public string? Developer { get; set; }
         public DateTime ReleaseDate { get; set; }
         public IEnumerable<string>? Categories { get; set; }
